d16ea585e81bc3a294e4e967ce0f53fc723e239a
irinaLee-hub <ileek08@gmail.com>
2020-12-21 12:13:28 -0500

    added to login page

--- src/test/java/com/fleetApps/utilities/BasePage.java
+++ src/test/java/com/fleetApps/utilities/BasePage.java
@@ -1,4 +1,10 @@
 package com.fleetApps.utilities;
 
-public class BasePage {
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class BasePage {
+
+public BasePage(){
+    PageFactory.initElements(Driver.getDriver(),this);
+}
 }
